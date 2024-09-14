08e5d7ccd37da84bdbb605b69e95037c1f4972a7
Darren Halpin <92470231+DH-GitRepos@users.noreply.github.com>
2024-09-14 23:24:06 +0100

    Refactor meta tags in layout.tsx and update social card image URL

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,16 @@ export default function RootLayout({
         <meta charSet="UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        {/* <!-- Primary Meta Tags --> */}
+        <meta name="title" content="Darren Halpin | Software Engineer" />
         <meta name="description" content="Resume website for Darren Halpin BSc (Hons)" />
+        {/* <!-- Open Graph - Social Sites --> */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://darren-halpin.web.app/" />
+        <meta property="og:title" content="Darren Halpin | Software Engineer" />
+        <meta property="og:description" content="I am a software engineer with a BSc (Hons) in Computer Science. This resume site details my skills and background." />
+        <meta property="og:image" content="https://darren-halpin.web.app/images/social_cards/SociaCard.png" />
+        {/* <!-- Icon data --> */}
         <meta name="msapplication-config" content="/images/favicon/browserconfig.xml" />
         <link rel="icon" type="image/x-icon" href="/images/favicon/favicon.ico" />
         <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-touch-icon.png" />
